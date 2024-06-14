a53333986ce1857e58c400fceeb381868fadd3fc
kuro-vale <jsalcedo218@gmail.com>
2024-06-13 23:34:03 -0500

    Add mobile navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,14 +2,17 @@ import {Component} from "@angular/core";
 import {MatIcon} from "@angular/material/icon";
 import {Icons} from "../constants";
 import {RouterModule} from "@angular/router";
+import {NgClass} from "@angular/common";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: "app-navbar",
   standalone: true,
-  imports: [MatIcon, RouterModule],
+  imports: [MatIcon, RouterModule, NgClass, MatIconButton],
   templateUrl: "./navbar.component.html",
-  styleUrl: "navbar.component.css"
+  styleUrl: "navbar.component.css",
 })
 export class NavbarComponent {
   protected readonly Icons = Icons;
+  protected focused = false;
 }
